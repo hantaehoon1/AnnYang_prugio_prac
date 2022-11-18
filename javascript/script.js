b18b15634a23f37285ad0fdbdd00ff2729694ae7
hantaehoon1 <hantaehun1@gmail.com>
2022-11-18 17:07:31 +0900

    2022-11-18

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function(){
   $('.menu>ul').mouseover(function(){
     $(this).find('.submenu').stop().slideDown(100);
-    $('header::before').stop().animate({height:'300px'})
+    $('#bgwrap').stop().animate({height:'300px'})
   }).mouseout(function(){
     $(this).find('.submenu').stop().slideUp(100);
-    $('header::before').stop().animate({height:'0px'})
+    $('#bgwrap').stop().animate({height:'100px'})
   });
 });
