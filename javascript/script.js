c6177c495b51e94b2de717efbd0d249fa63c1b33
hantaehoon1 <hantaehun1@gmail.com>
2022-11-21 14:56:28 +0900

    2022-11-21

--- javascript/script.js
+++ javascript/script.js
@@ -6,4 +6,34 @@ jQuery(document).ready(function(){
     $(this).find('.submenu').stop().slideUp(100);
     $('#bgwrap').stop().animate({height:'100px'})
   });
+
+  SliderBox1__init();
+  
+  function SliderBox1__init() {
+    const swiper = new Swiper(".slider-box-1 .swiper", {
+      // Optional parameters
+      loop: true,
+  
+      // If we need pagination
+      pagination: {
+        el: ".slider-box-1 .swiper-pagination"
+      },
+  
+      // Navigation arrows
+      navigation: {
+        nextEl: ".slider-box-1 .swiper-button-next",
+        prevEl: ".slider-box-1 .swiper-button-prev"
+      },
+    });
+  }
+  
+  swiper.on('slideChange', function(){
+    console.log(siwper.activeIndex);
+
+    $('.slider-box-1').removeClass('first-slide-actived');
+
+
+  });
+
+
 });
